f7fe18cf4d43034645e6ad684d798401d4bc86ce
kuldip2000 <157139786+kuldip2000@users.noreply.github.com>
2024-09-19 00:35:50 +0530

    changes.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -7,7 +7,7 @@ function TodoItem({ todoName, todoDate }) {
   return (
     <div className="container">
       <div className="row kg-row">
-        <div className="col-6">{todoName}</div>
+        <div className="col-6 kkk">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
           <button
